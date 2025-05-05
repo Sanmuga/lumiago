bb725251af29a7a58d463f2328d1570cfe1529a1
SANMUGAVADIVEL K <93981311+Sanmuga@users.noreply.github.com>
2025-05-05 10:17:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,4 +25,5 @@ def analyze_audio():
     return jsonify({"emotion": detected_emotion})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  
+    app.run(host='0.0.0.0', port=port, debug=True) 
